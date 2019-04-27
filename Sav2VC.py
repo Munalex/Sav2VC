a4596ce0ebbf42a20ed268a72fc8c32a41dd8ab4
Crash_Manucoot <manuelalvhevia@hotmail.com>
2019-04-27 12:05:15 +0200

    Add files via upload

---
+++ Sav2VC.py
@@ -0,0 +1,14 @@
+import sys
+
+
+if len(sys.argv) >= 1:
+    with open(sys.argv[1],"rb") as archivo:
+        with open("sav.dat","wb") as sav:
+            guardar = archivo.read(32784)
+            sav.write(guardar)
+
+        print("[+] Done! save.dat created")
+else:
+    print("error method of use Sav2VC.py file.sav")
+
+
